7be3d6bf0f4f6e88636697a41ea546d474418b82
Conrad Orta <COrtaDev@gmail.com>
2020-12-02 19:04:42 -0500

    Finished 2 methods: addToTail and removeFromTail

---
+++ AppAcademy/linkedLists/lib/demoList.js
@@ -0,0 +1,10 @@
+const { Node, LinkedList } = require('./linked_list.js');
+
+let myList = new LinkedList();
+
+myList.addToTail(375);
+myList.addToTail(1234);
+myList.addToTail(821);
+
+
+console.log(myList);
