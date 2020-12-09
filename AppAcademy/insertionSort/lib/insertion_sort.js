1a3409ebc29476acb745b1808b1eb2844b07deb9
Conrad Orta <corta.dev@gmail.com>
2020-12-09 18:07:27 -0500

    Update insertion_sort.js

--- AppAcademy/insertionSort/lib/insertion_sort.js
+++ AppAcademy/insertionSort/lib/insertion_sort.js
@@ -1,21 +1,12 @@
 function insertionSort(list) {
-// for i from 1 to length(list) inclusive do:
-
-//   /* select value to be inserted */
-//   valueToInsert = list[i]
-//   holePosition = i
-
-//   /* locate hole position for the element to be inserted */
-
-//   while holePosition > 0 and list[holePosition-1] > valueToInsert do:
-//     list[holePosition] = list[holePosition-1]
-//     holePosition = holePosition -1
-//   end while
-
-//   /* insert the number at hole position */
-//   list[holePosition] = valueToInsert
-
-// end for
+  for (let i = 1; i < list.length; i++) {
+    let current = list[i];
+    for (var j = i - 1; j >= 0 && current < list[j]; j--) {
+      list[j + 1] = list[j];
+    }
+    list[j + 1] = current;
+  }
+  return list;
 }
 
 module.exports = {
