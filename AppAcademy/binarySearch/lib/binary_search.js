89bcd16acdac8e1522edd6dddc240980db9e5118
Conrad Orta <corta.dev@gmail.com>
2020-12-10 18:47:43 -0500

    Update binary_search.js

--- AppAcademy/binarySearch/lib/binary_search.js
+++ AppAcademy/binarySearch/lib/binary_search.js
@@ -1,35 +1,18 @@
 function binarySearch(list, target) {
-  // parameter list: a list of sorted value
-  // parameter target: the value to search for
+  if (list.length === 0) {
+    return false;
+  }
+  let midIdx = Math.floor(list.length / 2);
+  let leftHalf = list.slice(0, midIdx);
+  let rightHalf = list.slice(midIdx + 1);
 
-  // if the list has zero length, then return false
-
-  // determine the slice point:
-  // if the list has an even number of elements,
-  //   the slice point is the number of elements
-  //   divided by two
-  // if the list has an odd number of elements,
-  //   the slice point is the number of elements
-  //   minus one divided by two
-
-  // create an list of the elements from 0 to the
-  //   slice point, not including the slice point,
-  //   which is known as the "left half"
-  // create an list of the elements from the
-  //   slice point to the end of the list which is
-  //   known as the "right half"
-
-  // if the target is less than the value in the
-  //   original array at the slice point, then
-  //   return the binary search of the "left half"
-  //   and the target
-
-  // if the target is greater than the value in the
-  //   original array at the slice point, then
-  //   return the binary search of the "right half"
-  //   and the target
-
-  // if neither of those is true, return true
+  if (target < list[midIdx]) {
+    return binarySearch(leftHalf, target);
+  } else if (target > list[midIdx]) {
+    return binarySearch(rightHalf, target);
+  } else {
+    return true;
+  }
 }
 
 function binarySearchIndex(list, target, low, high) {
