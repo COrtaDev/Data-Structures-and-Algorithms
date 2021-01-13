92b9ded16f0e9e3a4e25cca03dcc3e2856b5d919
Conrad Orta <corta.dev@gmail.com>
2021-01-13 09:25:54 -0500

    Create arrays.rb

---
+++ HackerRank/InterviewPrep/Ruby/arrays.rb
@@ -0,0 +1,7 @@
+# Initialize 3 variables here as explained in the problem statement
+array = Array.new
+# or array = []
+array_1 = Array.new(1)
+# array_1 = [nil]
+array_2 = Array.new(2, 10)
+# array_2 = [10, 10]
