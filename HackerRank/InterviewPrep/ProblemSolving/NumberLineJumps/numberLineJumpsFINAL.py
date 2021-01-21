42f9ce5e7e1cb7bda1a8a0efb34444e849785390
Conrad Orta <corta.dev@gmail.com>
2021-01-21 08:14:42 -0500

    Create numberLineJumpsFINAL.py

---
+++ HackerRank/InterviewPrep/ProblemSolving/NumberLineJumps/numberLineJumpsFINAL.py
@@ -0,0 +1,58 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def kangaroo(x1, v1, x2, v2):
+    # We will need to keep track of the number of jumps
+    # increment each iteration.
+    # calculate the distance each roo is jumping each iteration,
+    # if they converge print YES else print NO
+    k1 = x1 # these are the initial positions of each roo
+    k2 = x2
+    jumps = 0
+    # depending on where they start and their rate of change we can determine if they
+    # will ever even have a chance at converging.
+    if v1 == v2 and k1 != k2:
+        return "NO"
+    if (k1 > k2 and v1 > v2) or (k1 < k2 and v1 < v2):
+        return "NO"
+    if (k1 > k2):
+        while k1 > k2:
+            jumps += 1
+            k1 = (v1*(jumps)) + x1
+            k2 = (v2*(jumps)) + x2
+            if k1 == k2:
+                return "YES"
+        return "NO"
+    else:
+        while k1 < k2:
+            jumps += 1
+            k1 = (v1*(jumps)) + x1
+            k2 = (v2*(jumps)) + x2
+            if k1 == k2:
+                return "YES"
+        return "NO"
+    
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x1V1X2V2 = input().split()
+
+    x1 = int(x1V1X2V2[0])
+
+    v1 = int(x1V1X2V2[1])
+
+    x2 = int(x1V1X2V2[2])
+
+    v2 = int(x1V1X2V2[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
